cb337054c440f74c46b9e68dbd5aee66f5ab0684
russb78 <russell@russellbarnes.co.uk>
2013-11-13 17:40:07 +0000

    updated resultion checks

--- pi-mation/pi-mation.py
+++ pi-mation/pi-mation.py
@@ -5,32 +5,32 @@
 # Russell Barnes - 12 Nov 2013 for Linux User magazine issue 134 
 # www.linuxuser.co.uk
 
-# Just press F1 to get started!
-
 import pygame, os, sys
 import picamera
-from time import sleep
+import time
 from data import pyganim
 
 #define global variables
-pics_taken, pre_pic = 0, 0
+pics_taken = 0
 pics_list = []
 current_alpha, next_alpha = 255, 128
 
 # Initialise Pygame, start screen and camera
 pygame.init()
-res = pygame.display.list_modes() # 
-width, height = res[0] # automatically sets resolution to the highest / current option
-print "reported resolution:", width, "x", height
-if width > 1920 and height > 1080: # if bigger than max resolution of the pi, set it to the max
-    width, height = 1920, 1080 # if you're having problems manually set the resolution here
-start_pic = pygame.image.load(os.path.join('data', 'start_screen.jpg'))
-start_pic_fix = pygame.transform.scale(start_pic, (width, height))
-screen = pygame.display.set_mode([width, height])
-pygame.display.toggle_fullscreen()
-play_clock = pygame.time.Clock()
-camera = picamera.PiCamera()
-camera.resolution = (width, height)
+res = pygame.display.list_modes() # return the best resolution for your monitor
+width, height = res[0] # Having trouble getting the right resolution? Manually set with: 'width, height = 1650, 1050' (where the numbers match your monitor)
+print "Reported resolution is:", width, "x", height
+if width == 1600:
+    print "You appear to be trying to run Pi-Mation from the command prompt. Please try runnnig from the desktop (with 'startx') instead.\nQuitting..."
+    sys.exit(0)
+else:
+    start_pic = pygame.image.load(os.path.join('data', 'start_screen.jpg'))
+    start_pic_fix = pygame.transform.scale(start_pic, (width, height))
+    screen = pygame.display.set_mode([width, height])
+    pygame.display.toggle_fullscreen()
+    play_clock = pygame.time.Clock()
+    camera = picamera.PiCamera()
+    camera.resolution = (width, height)
 
 def take_pic():
     """Grabs an image and load it for the alpha preview and 
@@ -39,30 +39,31 @@ def take_pic():
     pics_taken += 1
     camera.capture(os.path.join('pics', 'image_' + str(pics_taken) + '.jpg'), use_video_port = True)
     pre_pic = pygame.image.load(os.path.join('pics', 'image_' + str(pics_taken) + '.jpg'))
-    pics_list.append((os.path.join('pics', 'image_' + str(pics_taken) + '.jpg'), 0.1))
+    pics_list.append((os.path.join('pics', 'image_' + str(pics_taken) + '.jpg'), 0.3))
 
 def delete_pic():
     """Doesn't actually delete the last picture, but the preview will 
     update and it will be successfully overwritten the next time you take a shot"""
     global pics_taken, pre_pic, pics_list
-    pics_taken -= 1
-    if len(pics_list) > 0:
-        del pics_list[-1]
     if pics_taken > 0:
+        pics_taken -= 1
+        del pics_list[-1]
+    if pics_taken >= 1:
         pre_pic = pygame.image.load(os.path.join('pics', 'image_' + str(pics_taken) + '.jpg'))
 
 def animate():
     """Do a quick live preview animation of 
     all current pictures taken using pyganim"""
     anim_obj = pyganim.PygAnimation(pics_list, loop=False)
+    screen.fill((0,0,0))
     camera.stop_preview()
     anim_obj.play()
     for pics in pics_list:
         anim_obj.blit(screen, (0, 0))
+        play_clock.tick(3.3)
         pygame.display.flip()
-        play_clock.tick(10)
     camera.start_preview()
-
+    
 def make_movie():
     """Quit out of the application 
     and create a movie with your pics"""
@@ -73,7 +74,6 @@ def make_movie():
     print "\nOnce complete, write 'omxplayer video.mp4' in the terminal to play your video.\n"
     fps = raw_input("How many frames per second do you want your video to be?\n--> ")
     os.system("avconv -r " + str(fps) + " -i " + str((os.path.join('pics', 'image_%d.jpg'))) + " -vcodec libx264 video.mp4")
-    sys.exit(0)
 
 def change_alpha():
     """Toggle's camera preview optimacy between half and full."""
@@ -88,7 +88,7 @@ def update_display():
     screen.fill((0,0,0))
     if pics_taken > 0:
         screen.blit(pre_pic, (0, 0))
-    pygame.display.update()
+    pygame.display.flip()
 
 def quit_app():
     """Cleanly closes the camera and the application"""
